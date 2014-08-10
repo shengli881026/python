68ca5192a7c4bf610e09b44e364a212dc6f97be8
shengli881026 <shengli881026@gmail.com>
2014-08-10 20:05:52 +0800

    test python
    test python

---
+++ dict.py
@@ -0,0 +1,46 @@
+# -*- coding: cp936 -*-
+l_dict ={'1':10,'2':15,'3':25,'4':30,'5':35};
+
+start = 10;
+end   = 30;
+
+# 从10-30 循环,然后循环字典中的数据,拼写sql;
+i = start;
+j =0
+while(i <= end):
+    print i;
+    for index in l_dict:
+        #index = str(index);
+        #print str(l_dict[index]);
+        #print '>>>>>----------------'+l_dict[index];
+        print '-------inset into test("id","value") values(\''+index+'\',\''+str(l_dict[index])+'\')';
+        j+=1;
+    i +=1;
+
+print 'count --- insert '+str(j);
+
+#for fruit in l_dict:
+   # print l_dict[fruit];
+
+for index in l_dict:
+    print '>>>---'+index;
+    print l_dict[index];
+
+for letter in 'python':
+    if letter == 'o':
+        #break;
+        #continue;
+        pass;
+        print 'o ----pass';
+    print '>>>>Current Letter :',letter;
+
+print 'end';
+
+
+# 根据计算的字符串的长度进行循环
+abc ='ssssssss';
+
+for ind in abc:
+    print ind;
+    #print '**********'+str(n);
+#print len(abc);
---
+++ test.py
@@ -0,0 +1,41 @@
+# -*- coding: cp936 -*-
+import sys
+print '>>> zhangshl';
+
+abc = '123'+\
+      '456'+\
+      '789';
+print abc;
+
+#print arr.lenth;
+
+#raw_input 接受用户输入
+#raw_input('logding....input....');
+
+#x='foo';sys.stdout.write(x+'\n');
+
+x = 2;
+
+if x==1:
+    print 'this --1 if';
+elif  x==2:
+    print 'thiis--2 if';
+
+#python string 字符串
+str  ="abcdefghigklmn";
+print str;
+print str[0];
+print str[1:];
+print str[1:4];
+print str*2;
+
+#python list 列表
+list_arr =['a','b','c','d','e','f','g','h','i','j','k'];
+print list_arr;
+print list_arr[1];
+print list_arr[3:];
+
+#python 元字典
+tdict = {'a':1,'b':2,'c':3};
+print tdict.values();
+
